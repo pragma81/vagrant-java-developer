2d76cd4f92b47e964287a7bc9030a9bba54d9271
Davide Antelmo <davide.antelmo@gmail.com>
2016-02-28 21:17:00 +0100

    aupdate docker-compose version

--- xubuntu-64-java-developer/docker.sh
+++ xubuntu-64-java-developer/docker.sh
@@ -16,5 +16,5 @@ update-rc.d docker.io defaults
 
 echo 'Install Docker Compose...'
 echo "------------------------"
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
